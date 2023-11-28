612f9d32b43f4044546988ae0de4a7583da15446
Simeon Dinev <s.dinev.j@gmail.com>
2023-11-28 16:38:18 +0200

    Add clubs page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Clubs from "./components/Clubs/Clubs";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/clubs" element={<Clubs />} />
       </Routes>
 
       <Footer />
