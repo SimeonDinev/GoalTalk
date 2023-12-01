9c53595c38f41dd3266a9cb1069f56682909120c
Simeon Dinev <s.dinev.j@gmail.com>
2023-12-01 12:54:20 +0200

    Add server

--- client/src/components/Clubs/Clubs.jsx
+++ client/src/components/Clubs/Clubs.jsx
@@ -1,24 +1,38 @@
+import { useState, useEffect } from "react";
+
 import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 import "./Clubs.css";
 
 function Clubs() {
+  const [club, setClub] = useState([]);
+
+  useEffect(() => {
+    fetch;
+  }, []);
+
   return (
     <Container fluid>
       <Row xs={1} md={2} className="g-6" style={{ padding: "40px" }}>
         {Array.from({ length: 6 }).map((_, idx) => (
           <Col key={idx} style={{ padding: "15px" }}>
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Title>Barcelona</Card.Title>
                 <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  FC Barcelona, founded in 1899, is a football giant based in
+                  Catalonia, Spain. Known for its iconic style of play and
+                  global success, Barcelona has become synonymous with
+                  excellence. From the historic Camp Nou to producing football
+                  legends through La Masia, the club's impact extends beyond the
+                  pitch, embodying the spirit of "Més que un club" (More than a
+                  club).
                 </Card.Text>
+                <Button variant="primary">Read More...</Button>
               </Card.Body>
             </Card>
           </Col>
---
+++ client/src/services/userService.js
@@ -0,0 +1,5 @@
+const baseURL = "http://localhost:3030/jsonstore";
+
+export const get = async () => {
+  `${baseURL}/goalTalk`;
+};
