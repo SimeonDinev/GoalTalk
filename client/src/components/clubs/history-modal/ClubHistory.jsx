d111e4797377b7ab4a636a327301b8dc3770dee3
Simeon Dinev <s.dinev.j@gmail.com>
2023-12-05 17:07:13 +0200

    Import global font-family

--- client/src/components/clubs/history-modal/ClubHistory.jsx
+++ client/src/components/clubs/history-modal/ClubHistory.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import * as service from "../../../services/clubService";
+import "./ClubHistory.css";
 
 function ClubHistory() {
   const [club, setClub] = useState({});
