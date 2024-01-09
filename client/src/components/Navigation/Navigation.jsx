fc2d4258625c1c97f7624abadbbd266c9285d7dd
Simeon Dinev <s.dinev.j@gmail.com>
2024-01-09 15:39:57 +0200

    Fix bug

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -8,44 +8,50 @@ import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/esm/Button";
 import Menu from "./nav-menu/Menu";
 
+import LogInModal from "./login-modal/LogInModal";
 import "../navigation/Navigation.css";
 
 const Navigation = () => {
-  const [isButtonHovered, setIsButttonHovered] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  if (show) {
+    var htmlElement = document.documentElement;
+    htmlElement.style.overflow = "disable";
+  } else {
+    var htmlElement = document.documentElement;
+    htmlElement.style.overflow = "visible";
+  }
 
   return (
-    <Row>
-      <Navbar expand="lg" id="navbar-wrapper">
-        <Container>
-          <Col md={2.5}>
-            <Navbar.Brand as={Link} to="/">
-              <span id="logo-goal">Goal</span>
-              <span id="logo-talk">Talk</span>
-            </Navbar.Brand>
-          </Col>
+    <>
+      {show && <LogInModal show={show} close={handleClose} />}
 
-          <Col md={7}>
-            <Menu />
-          </Col>
+      <Row style={{ margin: "0" }}>
+        <Navbar expand="lg" id="navbar-wrapper">
+          <Container>
+            <Col md={2.5}>
+              <Navbar.Brand as={Link} to="/">
+                <span id="logo-goal">Goal</span>
+                <span id="logo-talk">Talk</span>
+              </Navbar.Brand>
+            </Col>
 
-          <Col md={2.5} className="d-flex justify-content-end">
-            <Button
-              variant="success"
-              onMouseOver={() => setIsButttonHovered(true)}
-              onMouseOut={() => setIsButttonHovered(false)}
-              style={{
-                backgroundColor: isButtonHovered ? "#4CAF50" : "#28a745",
-                borderColor: "#4CAF50",
-                color: "#fff",
-                paddingBottom: "8.5px",
-              }}
-            >
-              Log-in
-            </Button>
-          </Col>
-        </Container>
-      </Navbar>
-    </Row>
+            <Col md={7}>
+              <Menu />
+            </Col>
+
+            <Col md={2.5} className="d-flex justify-content-end">
+              <Button id="login-btn" onClick={handleShow}>
+                Log-In
+              </Button>
+            </Col>
+          </Container>
+        </Navbar>
+      </Row>
+    </>
   );
 };
 
